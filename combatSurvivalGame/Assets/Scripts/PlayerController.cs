a763877b397d206f08a8a7531640a7d94dd08478
wapwya <lindajayana@mail.ru>
2019-03-19 22:55:16 +0700

    PlayerController
    Движение персонажа

---
+++ combatSurvivalGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private bool jump = false;
+    private Animator anim;
+    private bool noChao = false; // Пока
+    private Transform groundCheck;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+        groundCheck = gameObject.transform.Find("GroundCheck");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        noChao = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        
+        /* Jump */
+        if(Input.GetButtonDown("jump") && noChao)
+        {
+            jump = true;
+            anim.SetTrigger("pullou"); // jump animation
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        anim.SetFloat("");
+
+    }
+}
