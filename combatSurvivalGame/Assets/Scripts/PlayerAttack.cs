8371c3e2863de9cba731ec4ed66e2b86528cbea7
Александр Вертилецкий <Knight.siberia@gmail.com>
2019-03-27 22:57:29 +0700

    update
    Добавлена анимация атаки при нажатие "fire1", и много плюшек)

---
+++ combatSurvivalGame/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    Animator anim;
+
+    public float intervalDeAtaque;
+    private float proximoAtoque;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && Time.time > proximoAtoque)
+        {
+            Atacando();
+        }
+    }
+
+    void Atacando()
+    {
+        anim.SetTrigger("attackq");
+        proximoAtoque = Time.time + intervalDeAtaque;   
+    }
+}
